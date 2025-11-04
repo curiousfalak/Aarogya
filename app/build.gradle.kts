95115f1104f5b5d0e1643a6e79abb165058995cc
Anurag Kanojiya <anurag.kanojiya829@gmail.com>
2025-11-05 00:36:11 +0530

    chore: updated dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,7 @@ dependencies {
     implementation("io.coil-kt.coil3:coil-compose:3.3.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
     implementation("androidx.compose.material:material-icons-extended")
+
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
+
 }
